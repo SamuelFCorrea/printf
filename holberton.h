c9f30b45b4bc2bc37b6a38cf2587dbda2a827f0b
andresahumadaa <2298@holbertonschool.com>
2020-10-25 18:17:51 +0000

    printf

---
+++ holberton.h
@@ -0,0 +1,17 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ *struct types - estructura especifica para la funcion
+ * @p: puntero para caracteres
+ * @func : funcion para imprimir las funciones
+ */
+
+int _putchar(char c);
+int _printf(const char *format, ...);
