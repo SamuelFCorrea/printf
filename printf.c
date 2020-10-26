c5cfb72600ce1bebfb3d907c453e8217056d2b50
andresahumadaa <2298@holbertonschool.com>
2020-10-26 23:30:03 +0000

    printf

--- printf.c
+++ printf.c
@@ -11,3 +11,36 @@ unsigned int i = 0, characters_number = 0;
 int (*f)(va_list);
 if (!format)
 return (-1);
+va_start(list, format);
+for (i = 0; format[i] != '\0'; i++)
+{
+if (format[i] == '%')
+{
+if (format[i + 1] == '/0')
+return (-1);
+else if (format[i + 1] == '%')
+{
+_putchar('%');
+characters_number++;
+i++;
+}
+else if (cmp_func(format[i + 1]) != NULL)
+{
+characters_number += (cmp_func(format[i + 1]))(list);
+i++;
+}
+else
+{
+_putchar(format[i]);
+characters_number++;
+}
+}
+else
+{
+_putchar(format[i]);
+characters_number++;
+}
+}
+va_end(list);
+return (characters_number);
+}
