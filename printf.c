c9388dec639a845e10ff8ab16736dfe0f2ec9002
andresahumadaa <2298@holbertonschool.com>
2020-10-26 23:50:44 +0000

    printf

--- printf.c
+++ printf.c
@@ -44,3 +44,26 @@ characters_number++;
 va_end(list);
 return (characters_number);
 }
+/**
+ * get_op_func - punto
+ * @a: character
+ *
+ * Return: 0.
+ */
+int (*get_op_func(char s))(va_list)
+{
+format_t p_type[] = {
+{'c', print_char},
+{'s', print_string},
+{'\0', '\0'}
+};
+unsigned int j;
+for (j = 0; p_type[j].type != '\0'; j++)
+{
+if (s == p_type[j].type)
+{
+return (p_type[j].f);
+}
+}
+return (NULL);
+}
