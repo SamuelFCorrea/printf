b60c76b1977b0a7f7a01f6a0ec165641175e097b
Samuel Correa <samuelfelipecorrea@gmail.com>
2020-10-27 20:24:37 -0500

    commit

--- printf.c
+++ printf.c
@@ -1,4 +1,13 @@
 #include "holberton.h"
+
+/**
+ * get_op_func - get the correct function
+ * @c: input
+ *
+ * Return: on succes pointe to function
+ */
+
+
 int (*get_op_func(char c))(va_list)
 {
 format_t p_type[] = {
@@ -8,17 +17,22 @@ format_t p_type[] = {
 {'d', print_number},
 {'\0', NULL},
 };
-unsigned int j;
-for (j = 0; p_type[j].type != '\0'; j++)
-{
-if (s == p_type[j].type)
-{
-return (p_type[j].f);
-}
-}
-return (NULL);
+	unsigned int j;
+
+	for (j = 0; p_type[j].type; j++)
+		if (s == p_type[j].type)
+			return (p_type[j].f);
+
+	return (NULL);
 }
 
+
+/**
+ * _printf - our own printf
+ * @format: arguments
+ *
+ * Return: number of printed characters
+ */
 
 int _printf(const char *format, ...)
 {
