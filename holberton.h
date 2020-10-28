b675f88a717fc84373dd0f859f1deb2741496cf9
Samuel Correa <samuelfelipecorrea@gmail.com>
2020-10-27 19:05:02 -0500

    test

--- holberton.h
+++ holberton.h
@@ -17,15 +17,30 @@ typedef struct types
 {
 char *flags;
 int width;
-char *length;
+int precision;
+char length;
 char specifier;
-char *top;
-int (*f)();
-
+int charprinted;
 } new_t;
 
-int _putchar(char c);
-int _printf(const char *format, ...);
-int atoi(const char *str);
+
+int _putchar(char);
+int _printf(const char *, ...);
+int _atoi(const char *, new_t *);
+int _pre(const char *);
+new_t *_init(const char *);
+int def_type(const char *, new_t *, va_list);
+void _flags(const char *, int, new_t *);
+void _length(const char *, int, new_t *);
+void _printchar(va_list arg, new_t *);
+void _printinteger(va_list arg, new_t *);
+void _scientific(va_list arg, new_t *);
+void _printfloat(va_list arg, new_t *);
+void _printeshort(va_list arg, new_t *);
+void _printoctal(va_list arg, new_t *);
+void _printhexa(va_list arg, new_t *);
+void _printpointer(va_list arg, new_t *);
+char *duplicate(char *);
+
 
 #endif
--- printf.c
+++ printf.c
@@ -1,66 +1,35 @@
 #include "holberton.h"
-int (*get_op_func(char c))(va_list)
-{
-format_t p_type[] = {
-{'c', print_char},
-{'s', print_string},
-{'i', print_number},
-{'d', print_number},
-{'\0', NULL},
-};
-unsigned int j;
-for (j = 0; p_type[j].type != '\0'; j++)
-{
-if (s == p_type[j].type)
-{
-return (p_type[j].f);
-}
-}
-return (NULL);
-}
 
 /**
- * _printf - printf s,c and %
- * @format: print
- * Return: chars o total
+ * _printf - a try to do the same
+ * @format: values
+ *
+ * Return: number of characters printed
  */
+
 int _printf(const char *format, ...)
 {
-va_list list;
-unsigned int i = 0, characters_number = 0;
-int (*f)(va_list);
-if (!format)
-return (-1);
-va_start(list, format);
-for (i = 0; format[i] != '\0'; i++)
-{
-if (format[i] == '%')
-{
-if (format[i + 1] == '/0')
-return (-1);
-else if (format[i + 1] == '%')
-{
-_putchar('%');
-characters_number++;
-i++;
-}
-else if (cmp_func(format[i + 1]) != NULL)
-{
-characters_number += (cmp_func(format[i + 1]))(list);
-i++;
-}
-else
-{
-_putchar(format[i]);
-characters_number++;
-}
-}
-else
-{
-_putchar(format[i]);
-characters_number++;
-}
-}
-va_end(list);
-return (characters_number);
+	va_list arg;
+	int i, r, p = 0;
+	new_t *new;
+
+	new = malloc(sizeof(new_t));
+	if (new == NULL)
+		return (1);
+
+	va_start(arg, format);
+	for (i = 0; format && format[i]; i++)
+	{
+		if (format[i] == '%')
+			i += def_type(format + i, new, arg);
+		if (format[i] == '%')
+			i--;
+		else
+		{
+			write(1, format + i, 1);
+			p++;
+		}
+	}
+	r = p + (*new).charprinted;
+	return (r);
 }
